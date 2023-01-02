2171a7c08a07847aa93a702fb5ac2395adf8c1c5
fatima <fatimatuzahra837@gmai.com>
2023-01-02 12:55:07 +0500

    removed extra imports from init.py and config.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -10,7 +9,7 @@ app.config.from_object(Config)
 db = SQLAlchemy()
 db.init_app(app)
 migrate = Migrate(app, db)
-# login = LoginManager(app)
+
 
 from app import routes, models
 
