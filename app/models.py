3d876477640ca7ccbf7475d32d7302a895372067
Fatima Z <67465113+fatimah-z@users.noreply.github.com>
2023-01-11 10:19:09 +0500

    New branch (#1)
    * Updated code structure
    
    * Removed extra files
    
    * added missing response in login route
    
    * added gitignore
    
    * Pr changes implemented
    
    * removed untracked files
    
    * removed untracked files
    
    * added docker and docker-compose
    
    * fixed issues in volumes in docker-compose
    
    * removed extra imports from init.py and config.py
    
    * delete route
    
    * Added routes
    
    * migrations readded
    
    * improved models implementation and fixed other issues
    
    Co-authored-by: fatima <fatimatuzahra837@gmai.com>

---
+++ app/models.py
@@ -0,0 +1,36 @@
+from app import db
+import uuid
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password = db.Column(db.String(128))
+
+    def __int__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+
+class Car(db.Model):
+    __tablename__ = 'cars'
+
+    id = db.Column(db.String(36), primary_key=True, default=str(uuid.uuid4()))
+    objectId = db.Column(db.String(25), nullable=False)
+    createdAt = db.Column(db.String(25), nullable=False)
+    updatedAt = db.Column(db.String(25), nullable=False)
+    year = db.Column(db.INTEGER, nullable=True)
+    make = db.Column(db.String(20), nullable=True)
+    category = db.Column(db.String(25), nullable=True)
+
+    def __init__(self,objectId, createdAt, updatedAt, year, make, category):
+        self.objectId = objectId
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+        self.year = year
+        self.make = make
+        self.category = category
